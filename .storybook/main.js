5aadb9bef081afc5d8caa3fc9837b9140dfd3ce0
Oussama Haddouche <ohaddouche@sqli.com>
2026-04-17 22:25:28 +0100

    update deps versions

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-essentials'],
+  addons: ['@storybook/addon-docs'],
   framework: {
     name: '@storybook/react-vite',
     options: {},
